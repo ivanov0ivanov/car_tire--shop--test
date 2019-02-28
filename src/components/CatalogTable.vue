4b086714d31d5b9e835740364c3c45a27eaa1ff6
ivanov0ivanov <ivanov.ivanov8820@gmail.com>
2019-02-28 21:59:43 +0200

    table creation

---
+++ src/components/CatalogTable.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "CatalogTable"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CatalogTableItem.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "CatalogTableItem"
+    }
+</script>
+
+<style scoped>
+
+</style>
