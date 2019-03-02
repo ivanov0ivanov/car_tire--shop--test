53b3136b4f982bf1f9c32fa5d3f0388b02df2d2e
ivanov0ivanov <ivanov.ivanov8820@gmail.com>
2019-03-03 00:20:49 +0200

    item page creation

--- src/views/Item.vue
+++ src/views/Item.vue
@@ -8,31 +8,26 @@
         </div>
         <div class="item__container">
             <div class="item__img__wrapper">
-                <img class="item__img" src="../../public/items-img/tire_1.svg" alt="">
+                <img class="item__img" :src="`${this.$route.params.item.img}`" alt="">
             </div>
             <div class="item__container_data">
                 <div class="item__header">
                     <div class="item__title">
-                        <p>Premiorri Via Maggiore Z Plus</p>
+                        <p>{{this.$route.params.item.title}}</p>
                     </div>
                 </div>
                 <div class="item__body">
                     <div class="item__category__wrapper">
-                        <p>Category:<span>Зимние шины</span></p>
+                        <p>Category:<span>{{this.$route.params.item.category}}</span></p>
                     </div>
                     <div class="item__description">
-                        <p>
-                            Модель Debica Navigator 2 – это всесезонные шины, которые можно использовать круглый год. Они
-                            предназначены для спокойных водителей, которые на первое место ставят экономию топлива, а не
-                            быстрые разгоны или резкие торможения. Хорошие ходовые качества шины Debica Navigator 2
-                            обусловлены добавлением в резину силики, а также «S-образным» рисунком протектора.
-                        </p>
+                        <p>{{this.$route.params.item.description}}</p>
                     </div>
                 </div>
                 <div class="item__footer">
                     <div class="item__price">
                         <p>Цена:</p>
-                        <p>1000 <span>UAH</span></p>
+                        <p>{{this.$route.params.item.price}} <span>UAH</span></p>
                     </div>
                     <div class="item__btm__wrapper">
                         <button class="item__btn">Buy</button>
@@ -52,5 +47,4 @@
 <style lang="sass" scoped>
     p
      margin: 0
-    //{{this.$route.params.item.title}}
 </style>
